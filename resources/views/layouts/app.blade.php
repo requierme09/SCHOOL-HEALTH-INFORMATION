6fbbb59d52b7a1facdffb7f4204bb65d02075cb0
janssen <requiermejanssen@gmail.com>
2025-04-04 14:22:12 +0800

    added logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,75 +19,84 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-        rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
 
     <!-- Icons. Uncomment required icon fonts -->
     <link rel="stylesheet" href="{{ asset('Newfolder/assets/vendor/fonts/boxicons.css') }}" />
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href="{{ asset('Newfolder/assets/vendor/css/core.css') }}"
-        class="template-customizer-core-css" />
-    <link rel="stylesheet" href="{{ asset('Newdfolder/assets/vendor/css/theme-default.css') }}"
-        class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="{{ asset('Newfolder/assets/vendor/css/core.css') }}" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('Newdfolder/assets/vendor/css/theme-default.css') }}" class="template-customizer-theme-css" />
     <link rel="stylesheet" href="{{ asset('Newfolder/assets/css/demo.css') }}" />
 
     <!-- Vendors CSS -->
     <link rel="stylesheet" href="{{ asset('Newfolder/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
-
     <link rel="stylesheet" href="{{ asset('Newfolder/assets/vendor/libs/apex-charts/apex-charts.css') }}" />
-    <link rel="stylesheet" href="{{asset('Newfolder/assets/vendor/css/pages/page-auth.css')}}">
+    <link rel="stylesheet" href="{{ asset('Newfolder/assets/vendor/css/pages/page-auth.css') }}">
 
     <!-- Page CSS -->
 
     <!-- Helpers -->
     <script src="{{ asset('Newfolder/assets/vendor/js/helpers.js') }}"></script>
 
-    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="{{ asset('Newfolder/assets/js/config.js') }}"></script>\
+    <!-- Template customizer & Theme config -->
+    <script src="{{ asset('Newfolder/assets/js/config.js') }}"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS for logout positioning -->
+    <style>
+        .logout-container {
+            position: fixed;
+            top: 10px;
+            right: 10px;
+            z-index: 999;
+        }
+
+        .logout-container button {
+            background: none;
+            border: none;
+            color: #333;
+            font-size: 16px;
+            cursor: pointer;
+        }
+    </style>
 
 </head>
 
 <body>
-    <!-- {{-- <div id="app">
-        <main class="py-4">
-            @yield('content')
-        </main>
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        @if (Route::has('login'))
+            <div class="logout-container">
+                @auth
+                    <!-- Logout Button -->
+                    <form action="{{ route('logout') }}" method="POST" style="display: inline;">
+                        @csrf
+                        <button type="submit" class="text-sm text-gray-700 dark:text-gray-500 underline">Logout</button>
+                    </form>
+                @else
+                    <!-- You can add login button here if needed -->
+                @endauth
+            </div>
+        @endif
     </div>
-    </nav>
 
-
-    </div> --}} -->
     <div id="app">
         <main class="py-4">
             @yield('content')
         </main>
     </div>
-  
-    <script src="{{ asset('Newfolder/assets/vendor/libs/jquery/jquery.js') }}"></script>
 
+    <script src="{{ asset('Newfolder/assets/vendor/libs/jquery/jquery.js') }}"></script>
     <script src="{{ asset('Newfolder/assets/vendor/libs/popper/popper.js') }}"></script>
     <script src="{{ asset('Newfolder/assets/vendor/js/bootstrap.js') }}"></script>
-
-
-
-
     <script src="{{ asset('Newfolder/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
-
     <script src="{{ asset('Newfolder/assets/vendor/js/menu.js') }}"></script>
-
-    <!-- endbuild -->
 
     <!-- Vendors JS -->
     <script src="{{ asset('Newfolder/assets/vendor/libs/apex-charts/apexcharts.js') }}"></script>
 
     <!-- Main JS -->
-
     <script src="{{ asset('Newfolder/assets/js/main.js') }}"></script>
-
 
     <!-- Page JS -->
     <script src="{{ asset('Newfolder/assets/js/dashboards-analytics.js') }}"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,19 +22,25 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+        @if (Route::has('login'))
+    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+        @auth
+            <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+            <!-- Add the Logout link here -->
+            <form action="{{ route('logout') }}" method="POST" style="display: inline;">
+                @csrf
+                <button type="submit" class="text-sm text-gray-700 dark:text-gray-500 underline">Logout</button>
+            </form>
+        @else
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
             @endif
+        @endauth
+    </div>
+@endif
+
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
